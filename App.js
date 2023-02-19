04edb4dde8bcc61ddb84560f8de83523efee67f3
moraj <dhwajmoraj7@gmail.com>
2023-02-19 18:01:47 +0530

    Learn native components.

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import {ActivityIndicator, Alert, Button, StyleSheet, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Alert,
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 const App = () => (
   <View style={[styles.container, styles.horizontal]}>
@@ -11,6 +19,11 @@ const App = () => (
       title="Press me"
       color="#f194ff"
       onPress={() => Alert.alert('Button with adjusted color pressed')}
+    />
+    <FlatList
+      data={DATA}
+      renderItem={({item}) => <Item title={item.title} />}
+      keyExtractor={item => item.id}
     />
   </View>
 );
@@ -25,6 +38,35 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     padding: 10,
   },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
 });
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 export default App;
