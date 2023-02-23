33abccf95f566646ad110796e30ba24ccd99a3bd
moraj <dhwajmoraj7@gmail.com>
2023-02-23 19:00:01 +0530

    Added APIs and Components.

--- App.js
+++ App.js
@@ -9,6 +9,14 @@ import DocumentScreen from './screens/DocumentScreen';
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import VideoScreen from './screens/VideoScreen';
 import TabNavigator from './navigators/TabNavigator';
+import AppStateScreen from './screens/AppStateScreen';
+import DimentionsScreen from './screens/DimenstionScreen';
+import EasingScreen from './screens/EasingScreen';
+import InteractionManagerScreen from './screens/InteractionManagerScreen';
+import DevSettingScreen from './screens/DevSettingScreen';
+import KeyBoardScreen from './screens/KeyBoardScreen';
+import LayoutAnimationScreen from './screens/LayoutAnimationScreen';
+import PanResponderScreen from './screens/PanResponderScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -31,6 +39,20 @@ function App() {
           <Stack.Screen name="Videos" component={VideoScreen} />
           <Stack.Screen name="Tests" component={TestScreen} />
           <Stack.Screen name="Documents" component={DocumentScreen} />
+          <Stack.Screen name="AppState" component={AppStateScreen} />
+          <Stack.Screen name="DevSetting" component={DevSettingScreen} />
+          <Stack.Screen name="Dimentions" component={DimentionsScreen} />
+          <Stack.Screen name="Easing" component={EasingScreen} />
+          <Stack.Screen name="KeyBoard" component={KeyBoardScreen} />
+          <Stack.Screen name="PanResponder" component={PanResponderScreen} />
+          <Stack.Screen
+            name="LayoutAnimation"
+            component={LayoutAnimationScreen}
+          />
+          <Stack.Screen
+            name="InteractionManager"
+            component={InteractionManagerScreen}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </ApolloProvider>
